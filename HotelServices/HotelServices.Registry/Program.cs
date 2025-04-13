4f7c665aa4059329a18fdb817fd9be2fa4210b95
lahiruomadhawa <lahirumadhawa00@gmail.com>
2025-04-13 15:30:47 +1000

    Restructure repository - Created hotel services

---
+++ HotelServices/HotelServices.Registry/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelServices.Registry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
